ee9c1117f7009063ba6205e99290e89b98842c48
tac <514162920@qq.com>
2017-10-27 15:34:03 +0800

    banner sample

---
+++ banner_sample/src/main/java/cn/tac/test/spring/boot/banner/BannerApplication.java
@@ -0,0 +1,15 @@
+package cn.tac.test.spring.boot.banner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tac
+ * @since 27/10/2017
+ */
+@SpringBootApplication
+public class BannerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BannerApplication.class, args);
+    }
+}
