b3427e1f0c631f71e344d95bcd5d8fa56acf7a7e
tac <514162920@qq.com>
2017-10-31 22:38:28 +0800

    configuration file location test

--- exter_config_sampple/src/test/java/cn/tac/test/spring/boot/extconfig/LocationTest.java
+++ exter_config_sampple/src/test/java/cn/tac/test/spring/boot/extconfig/LocationTest.java
@@ -1,27 +1,29 @@
 package cn.tac.test.spring.boot.extconfig;
 
 import cn.tac.test.spring.boot.extconfig.common.ExtConfigApplication;
-import cn.tac.test.spring.boot.extconfig.common.RandomBean;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 
 /**
  * @author tac
  * @since 31/10/2017
  */
-
 @SpringBootTest(classes = ExtConfigApplication.class)
 @RunWith(SpringJUnit4ClassRunner.class)
-public class RandomValueTest {
-    @Autowired
-    private RandomBean bean;
+public class LocationTest {
+
+    @Value("${from.config}")
+    private String fromConfig;
+
+    @Value("${custom.config}")
+    private String customConfig;
 
     @Test
     public void testSimply() {
-        System.out.println(bean);
+        System.out.println(fromConfig);
+        System.out.println(customConfig);
     }
 }
