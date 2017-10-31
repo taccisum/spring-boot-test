de105926d4a3088ae496064e65347508e1d0dce9
tac <514162920@qq.com>
2017-10-31 23:14:40 +0800

    placeholder test

---
+++ exter_config_sampple/src/test/java/cn/tac/test/spring/boot/extconfig/PlaceholderTest.java
@@ -0,0 +1,25 @@
+package cn.tac.test.spring.boot.extconfig;
+
+import cn.tac.test.spring.boot.extconfig.common.ExtConfigApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author tac
+ * @since 31/10/2017
+ */
+@SpringBootTest(classes = ExtConfigApplication.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class PlaceholderTest {
+
+    @Value("${my.placeholder}")
+    private String placeholder;
+
+    @Test
+    public void testSimply() {
+        System.out.println(placeholder);
+    }
+}
