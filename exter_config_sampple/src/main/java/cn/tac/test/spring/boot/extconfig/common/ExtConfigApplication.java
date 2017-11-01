571696e5b802111ccbdd793f12cb54a59775bc00
tac <514162920@qq.com>
2017-11-01 14:56:10 +0800

    shipped with metadata

--- exter_config_sampple/src/main/java/cn/tac/test/spring/boot/extconfig/common/ExtConfigApplication.java
+++ exter_config_sampple/src/main/java/cn/tac/test/spring/boot/extconfig/common/ExtConfigApplication.java
@@ -8,6 +8,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class ExtConfigApplication {
-    public static void main(String[] args) {
-    }
 }
