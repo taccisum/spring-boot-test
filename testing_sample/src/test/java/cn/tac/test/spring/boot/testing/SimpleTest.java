9e5efaa319c2856667d719afa0ce224ccbfa9166
tac <514162920@qq.com>
2017-11-10 14:21:20 +0800

    optimize

--- testing_sample/src/test/java/cn/tac/test/spring/boot/testing/SimpleTest.java
+++ testing_sample/src/test/java/cn/tac/test/spring/boot/testing/SimpleTest.java
@@ -1,9 +1,9 @@
 package cn.tac.test.spring.boot.testing;
 
-import cn.tac.test.spring.boot.testing.common.CommonTestingApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,7 +12,8 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @author tac
  * @since 10/11/2017
  */
-@SpringBootTest(classes = CommonTestingApplication.class)
+@SpringBootTest
+@SpringBootApplication
 @RunWith(SpringRunner.class)
 public class SimpleTest {
     @Autowired
